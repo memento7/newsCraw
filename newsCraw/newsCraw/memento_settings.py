4f5267093bc028ec7f5acde963d486a70715407f
MaybeS <maytryark@gmail.com>
2017-06-06 21:57:31 +0900

    Update elasticsearch settings

--- newsCraw/newsCraw/memento_settings.py
+++ newsCraw/newsCraw/memento_settings.py
@@ -5,6 +5,7 @@ SERVER_ES = 'server2.memento.live'
 SERVER_ES_INFO = {
     'host': SERVER_ES,
     'port': 9200,
+    'http_auth': (environ['MEMENTO_ELASTIC'], environ['MEMENTO_ELASTIC_PASS'])
 }
 SERVER_API = 'https://api.memento.live/persist/'
 SERVER_API_HEADER = {
