7fc0dabbd69d6fabfdd326913de3ecd9d3173c33
MaybeS <maytryark@gmail.com>
2017-06-06 18:15:34 +0900

    Update for installation

---
+++ newsCraw/install_nltk_data.py
@@ -0,0 +1,7 @@
+from nltk import download
+download('averaged_perceptron_tagger')
+download('maxent_ne_chunker')
+download('maxent_treebank_pos_tagger')
+download('punkt')
+download('treebank')
+download('words')
