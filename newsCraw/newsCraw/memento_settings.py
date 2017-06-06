24dcfdcbda4378ded5e74d5322c723070b99b485
MaybeS <maytryark@gmail.com>
2017-06-06 22:05:29 +0900

    Update memento_settings.py

--- newsCraw/newsCraw/memento_settings.py
+++ newsCraw/newsCraw/memento_settings.py
@@ -7,9 +7,3 @@ SERVER_ES_INFO = {
     'port': 9200,
     'http_auth': (environ['MEMENTO_ELASTIC'], environ['MEMENTO_ELASTIC_PASS'])
 }
-SERVER_API = 'https://api.memento.live/persist/'
-SERVER_API_HEADER = {
-    "Content-Type" : "application/json",
-    "charset": "utf-8",
-    "Authorization": environ['MEMENTO_BASIC']
-}
